63d0d54b34b40b2801388b884b6f7403337251a4
Mads Hougesen <madshougesen@gmail.com>
2020-05-07 10:48:11 +0200

    form

---
+++ scrips/js.js
@@ -0,0 +1,12 @@
+function autoFill() {
+    document.getElementById("fyldningsHeight").value = "300";
+    document.getElementById("fyldningsProcent").value = "70";
+    document.getElementById("tilladtUdhæng").value = "0";
+    document.getElementById("palleBredde").value = "800";
+    document.getElementById("palleHeight").value = "144";
+    document.getElementById("palleLength").value = "1200";
+    document.getElementById("senorMaxPosMar").value = "0";
+    document.getElementById("senorMaxNegMar").value = "0";
+
+
+}
