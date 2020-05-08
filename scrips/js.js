103fbb4c41fce8db612c0c021cf4ced39cc64e5d
Abdjul <60339265+Abdjulaziz@users.noreply.github.com>
2020-05-08 02:27:25 +0200

    login input

--- scrips/js.js
+++ scrips/js.js
@@ -1,25 +1,31 @@
 function autoFill() {
-    document.getElementById("fyldningsHeight").value = "300";
-    document.getElementById("fyldningsProcent").value = "70";
-    document.getElementById("tilladtUdhæng").value = "0";
-    document.getElementById("palleBredde").value = "800";
-    document.getElementById("palleHeight").value = "144";
-    document.getElementById("palleLength").value = "1200";
-    document.getElementById("senorMaxPosMar").value = "0";
-    document.getElementById("senorMaxNegMar").value = "0";
+  document.getElementById("fyldningsHeight").value = "300";
+  document.getElementById("fyldningsProcent").value = "70";
+  document.getElementById("tilladtUdhæng").value = "0";
+  document.getElementById("palleBredde").value = "800";
+  document.getElementById("palleHeight").value = "144";
+  document.getElementById("palleLength").value = "1200";
+  document.getElementById("senorMaxPosMar").value = "0";
+  document.getElementById("senorMaxNegMar").value = "0";
+}
+function check(form) {
+  if (form.username.value == "robotnordic" && form.password.value == "123") {
+    window.open("dashboard.html");
+  } else {
+  }
 }
 
 function openProduktInfo(evt, produktInfo) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(produktInfo).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(produktInfo).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
