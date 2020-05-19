8069b25d096007ad2e8372e105dd3368b6f60674
Abdjul <60339265+Abdjulaziz@users.noreply.github.com>
2020-05-19 10:20:51 +0200

    Burger

---
+++ styleguide/scripts/app.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".navi");
+    const main = document.querySelector(".maini");
+
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("toggle");
+        if (nav.style.width) {
+            nav.style.width = "";
+            main.style.marginLeft= "";
+            nav.style.borderRight = ""
+        } else {
+            nav.style.width = "250px";
+            main.style.marginLeft = "250px"
+            nav.style.borderRight = "5px solid #F15A22";
+        }
+    });
+};
+navSlide();
